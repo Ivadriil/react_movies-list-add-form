1e5edf3b17e57f38485574c18c7e51b24cb82c58
Ivadriil <petrivi51@gmail.com>
2026-04-28 18:33:45 +0300

    add task solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,135 @@
+import React from 'react';
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [title, setTitle] = useState('');
+  const [hasTitleError, setHasTitleError] = useState(false);
+
+  const [imgUrl, setImgUrl] = useState('');
+  const [hasImgUrlError, setHasImgUrlError] = useState(false);
+
+  const [description, setDescription] = useState('');
+
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [hasImdbUrlError, setHasImdbUrlError] = useState(false);
+
+  const [imdbId, setImdbId] = useState('');
+  const [hasImdbIdError, setHasImdbIdError] = useState(false);
+
+  const handTitle = (value: string) => {
+    setTitle(value);
+    setHasTitleError(false);
+  };
+
+  const handhasImgUr = (value: string) => {
+    setImgUrl(value);
+    setHasImgUrlError(false);
+  };
+
+  const handImdbUrl = (value: string) => {
+    setImdbUrl(value);
+    setHasImdbUrlError(false);
+  };
+
+  const handImdbId = (value: string) => {
+    setImdbId(value);
+    setHasImdbIdError(false);
+  };
+
+  const pattern = new RegExp(
+    '^' +
+      '((([A-Za-z]{3,9}:(?:\\/\\/)?)' +
+      '(?:[-;:&=+$,\\w]+@)?' +
+      '[A-Za-z0-9.-]+' +
+      '|(?:www\\.|[-;:&=+$,\\w]+@)' +
+      '[A-Za-z0-9.-]+)' +
+      '((?:\\/[+~%/.\\w-_]*)?' +
+      '\\??' +
+      '(?:[-+=&;%@,.\\w_]*)' +
+      '#?' +
+      '(?:[,.!/\\\\\\w]*))?)' +
+      '$',
+  );
+  const handSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    setHasTitleError(!title.trim());
+    setHasImgUrlError(!pattern.test(imgUrl));
+    setHasImdbUrlError(!pattern.test(imdbUrl));
+    setHasImdbIdError(!imdbId.trim());
+
+    if (
+      !title.trim() ||
+      !pattern.test(imgUrl) ||
+      !pattern.test(imdbUrl) ||
+      !imdbId.trim()
+    ) {
+      return;
+    }
+
+    onAdd({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    });
+
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" onSubmit={handSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
-        required
+        value={title}
+        onChange={handTitle}
+        required={!hasTitleError}
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={description}
+        onChange={setDescription}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={imgUrl}
+        onChange={handhasImgUr}
+        required={!hasImgUrlError}
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={imdbUrl}
+        onChange={handImdbUrl}
+        required={!hasImdbUrlError}
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={imdbId}
+        onChange={handImdbId}
+        required={!hasImdbIdError}
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +137,12 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={
+              !title.trim() ||
+              !pattern.test(imgUrl) ||
+              !pattern.test(imdbUrl) ||
+              !imdbId.trim()
+            }
           >
             Add
           </button>
